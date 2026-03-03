678fbcae38ce7f9437d4c4e388a240831048290f
Pragati4214 <pragatipandey.10.2005@gmail.com>
2026-03-03 20:18:29 +0530

    Connected to MongoDb and added user model

--- server/index.js
+++ server/index.js
@@ -1,9 +1,15 @@
-import express from 'express';
-const port = 8000;
-const app = express();
-app.listen(port, () => {
-    console.log(`Server is running on port ${port}`);
-});
+import express from 'express'
+import dotenv from 'dotenv'
+import connectDb from './utils/connectDb.js'
+dotenv.config()
+
+const app = express()
+const PORT = process.env.PORT || 5000
+
 app.get("/", (req, res) => {
     res.json({ message: 'PrepBuddy Server is running!' });
+});
+app.listen(PORT, () => {
+    console.log(`Server is running on port ${PORT}`);
+    connectDb
 })
