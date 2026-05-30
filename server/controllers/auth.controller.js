5cfa88004b2ec35d1fbece36daf0ebdaeb792154
Pragati4214 <pragatipandey.10.2005@gmail.com>
2026-05-30 22:51:53 +0530

    Update cookie settings for token security

--- server/controllers/auth.controller.js
+++ server/controllers/auth.controller.js
@@ -22,8 +22,9 @@ export const googleAuth = async (req, res) => {
 
     res.cookie("token", token, {
       httpOnly: true,
-      secure: false,
-      sameSite: "lax",
+      secure: true,
+      sameSite: "none",
+      path:"/",
       maxAge: 7 * 24 * 60 * 60 * 1000,
     });
 
